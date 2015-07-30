6c1d66f9cc1dc24c0de83c1256b9e91a51b62772
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2015-07-30 07:56:31 -0500

    Wrap in class exists check

--- wpcom-legacy-redirector.php
+++ wpcom-legacy-redirector.php
@@ -28,117 +28,126 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require( __DIR__ . '/includes/wp-cli.php' );
 }
 
-class WPCOM_Legacy_Redirector {
-	const POST_TYPE = 'vip-legacy-redirect';
-	const CACHE_GROUP = 'vip-legacy-redirect-2';
+if ( ! class_exists( 'WPCOM_Legacy_Redirector' ) ) {
+	class WPCOM_Legacy_Redirector {
+		const POST_TYPE = 'vip-legacy-redirect';
+		const CACHE_GROUP = 'vip-legacy-redirect-2';
 
-	static function start() {
-		add_action( 'init', array( __CLASS__, 'init' ) );
-		add_filter( 'template_redirect', array( __CLASS__, 'maybe_do_redirect' ), 0 ); // hook in early, before the canonical redirect
-	}
+		static function start() {
+			add_action( 'init', array( __CLASS__, 'init' ) );
+			add_filter( 'template_redirect', array(
+				__CLASS__,
+				'maybe_do_redirect'
+			), 0 ); // hook in early, before the canonical redirect
+		}
 
-	static function init() {
-		register_post_type( self::POST_TYPE, array(
-			'public' => false,
-		) );
-	}
+		static function init() {
+			register_post_type( self::POST_TYPE, array(
+				'public' => false,
+			) );
+		}
 
-	static function maybe_do_redirect() {
-		// Avoid the overhead of running this on every single pageload.
-		// We move the overhead to the 404 page but the trade-off for site performance is worth it.
-		if ( ! is_404() )
-			return;
-
-		$url = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
-
-		if ( ! empty( $_SERVER['QUERY_STRING'] ) )
-			$url .= '?' . $_SERVER['QUERY_STRING'];
-
-		$request_path = apply_filters( 'wpcom_legacy_redirector_request_path', $url );
-
-		if ( $request_path ) {
-			$redirect_uri = self::get_redirect_uri( $request_path );
-
-			if ( $redirect_uri ) {
-				header( 'X-legacy-redirect: HIT' );
-				wp_safe_redirect( $redirect_uri, 301 );
-				exit;
+		static function maybe_do_redirect() {
+			// Avoid the overhead of running this on every single pageload.
+			// We move the overhead to the 404 page but the trade-off for site performance is worth it.
+			if ( ! is_404() ) {
+				return;
 			}
-		}
-	}
 
-	/**
-	 *
-	 * @param string $from_url URL or path that should be redirected; should have leading slash if path.
-	 * @param int|string $redirect_to The post ID or URL to redirect to.
-	 * @return bool|WP_Error Error if invalid redirect URL specified; true otherwise.
-	 */
-	static function insert_legacy_redirect( $from_url, $redirect_to ) {
+			$url = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
 
-		$from_url = parse_url( $from_url, PHP_URL_PATH );
-		$from_url_hash = self::get_url_hash( $from_url );
+			if ( ! empty( $_SERVER['QUERY_STRING'] ) ) {
+				$url .= '?' . $_SERVER['QUERY_STRING'];
+			}
 
-		$args = array(
-			'post_name' => $from_url_hash,
-			'post_title' => $from_url,
-			'post_type' => self::POST_TYPE,
-		);
+			$request_path = apply_filters( 'wpcom_legacy_redirector_request_path', $url );
 
-		if ( is_numeric( $redirect_to ) ) {
-			$args['post_parent'] = $redirect_to;
-		} elseif ( false !== parse_url( $redirect_to ) ) {
-			$args['post_excerpt'] = esc_url_raw( $redirect_to );
-		} else {
-			return new WP_Error( 'invalid-redirect-url', 'Invalid redirect_to param; should be a post_id or a URL' );
-		}
+			if ( $request_path ) {
+				$redirect_uri = self::get_redirect_uri( $request_path );
 
-		wp_insert_post( $args );
-
-		wp_cache_delete( $from_url_hash, self::CACHE_GROUP );
-
-		return true;
-	}
-
-	static function get_redirect_uri( $url ) {
-		$url = urldecode( $url );
-		$url_hash = self::get_url_hash( $url );
-
-		$redirect_post_id = wp_cache_get( $url_hash, self::CACHE_GROUP );
-
-		if ( false === $redirect_post_id ) {
-			$redirect_post_id = self::get_redirect_post_id( $url );
-			wp_cache_add( $url_hash, $redirect_post_id, self::CACHE_GROUP );
-		}
-
-		if ( $redirect_post_id ) {
-			$redirect_post = get_post( $redirect_post_id );
-
-			if ( 0 !== $redirect_post->post_parent ) {
-				return get_permalink( $redirect_post->post_parent );
-			} elseif ( ! empty( $redirect_post->post_excerpt ) ) {
-				return esc_url_raw( $redirect_post->post_excerpt );
+				if ( $redirect_uri ) {
+					header( 'X-legacy-redirect: HIT' );
+					wp_safe_redirect( $redirect_uri, 301 );
+					exit;
+				}
 			}
 		}
 
-		return false;
+		/**
+		 *
+		 * @param string     $from_url    URL or path that should be redirected; should have leading slash if path.
+		 * @param int|string $redirect_to The post ID or URL to redirect to.
+		 *
+		 * @return bool|WP_Error Error if invalid redirect URL specified; true otherwise.
+		 */
+		static function insert_legacy_redirect( $from_url, $redirect_to ) {
+
+			$from_url      = parse_url( $from_url, PHP_URL_PATH );
+			$from_url_hash = self::get_url_hash( $from_url );
+
+			$args = array(
+				'post_name'  => $from_url_hash,
+				'post_title' => $from_url,
+				'post_type'  => self::POST_TYPE,
+			);
+
+			if ( is_numeric( $redirect_to ) ) {
+				$args['post_parent'] = $redirect_to;
+			} elseif ( false !== parse_url( $redirect_to ) ) {
+				$args['post_excerpt'] = esc_url_raw( $redirect_to );
+			} else {
+				return new WP_Error( 'invalid-redirect-url', 'Invalid redirect_to param; should be a post_id or a URL' );
+			}
+
+			wp_insert_post( $args );
+
+			wp_cache_delete( $from_url_hash, self::CACHE_GROUP );
+
+			return true;
+		}
+
+		static function get_redirect_uri( $url ) {
+			$url      = urldecode( $url );
+			$url_hash = self::get_url_hash( $url );
+
+			$redirect_post_id = wp_cache_get( $url_hash, self::CACHE_GROUP );
+
+			if ( false === $redirect_post_id ) {
+				$redirect_post_id = self::get_redirect_post_id( $url );
+				wp_cache_add( $url_hash, $redirect_post_id, self::CACHE_GROUP );
+			}
+
+			if ( $redirect_post_id ) {
+				$redirect_post = get_post( $redirect_post_id );
+
+				if ( 0 !== $redirect_post->post_parent ) {
+					return get_permalink( $redirect_post->post_parent );
+				} elseif ( ! empty( $redirect_post->post_excerpt ) ) {
+					return esc_url_raw( $redirect_post->post_excerpt );
+				}
+			}
+
+			return false;
+		}
+
+		static function get_redirect_post_id( $url ) {
+			global $wpdb;
+
+			$url_hash = self::get_url_hash( $url );
+
+			$redirect_post_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_type = %s AND post_name = %s LIMIT 1", self::POST_TYPE, $url_hash ) );
+
+			if ( ! $redirect_post_id ) {
+				$redirect_post_id = 0;
+			}
+
+			return $redirect_post_id;
+		}
+
+		private static function get_url_hash( $url ) {
+			return md5( $url );
+		}
 	}
 
-	static function get_redirect_post_id( $url ) {
-		global $wpdb;
-
-		$url_hash = self::get_url_hash( $url );
-
-		$redirect_post_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_type = %s AND post_name = %s LIMIT 1", self::POST_TYPE, $url_hash ) );
-
-		if ( ! $redirect_post_id )
-			$redirect_post_id = 0;
-
-		return $redirect_post_id;
-	}
-
-	private static function get_url_hash( $url ) {
-		return md5( $url );
-	}
+	WPCOM_Legacy_Redirector::start();
 }
-
-WPCOM_Legacy_Redirector::start();
